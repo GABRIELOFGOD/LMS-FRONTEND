68d74062a78b222685a1ea6e2adf59165f01527b
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2025-05-21 23:17:51 +0100

    GREAT IS THY FAITHFULNESS LORD UNTO ME!

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -3,11 +3,13 @@ import Hero from '@/components/layout/home/hero';
 import HowItWorks from '@/components/layout/home/how-it-works';
 import React from 'react';
 import Testimony from '../../components/layout/home/students-testimony';
+import About from '@/components/layout/home/home-about';
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <About />
       <CoursePreview />
       <HowItWorks />
       <Testimony />
