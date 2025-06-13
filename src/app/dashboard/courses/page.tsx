014f381894a1494e395686459d640253e0effb3b
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2025-06-13 13:12:39 +0100

    GREAT IS THY FAITHFULNESS LORD UNTO ME!

--- src/app/dashboard/courses/page.tsx
+++ src/app/dashboard/courses/page.tsx
@@ -1,6 +1,17 @@
+import { Button } from "@/components/ui/button";
+
 const DashboardCourses = () => {
   return (
-    <div>DashboardCourses</div>
+    <div className="flex flex-col px-3 md:px-5 py-10 gap-5">
+      <div className="w-full flex justify-between">
+        <p className="font-bold text-2xl">Courses</p>
+        <Button
+          className=""
+        >
+          Create Course
+        </Button>
+      </div>
+    </div>
   )
 }
 export default DashboardCourses;
